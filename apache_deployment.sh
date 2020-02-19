c9710a3f24b4398408fea6808b03180aac3c5dbe
Johannes Unglert <unglert@b1-systems.de>
2020-02-19 11:18:30 +0100

    2nd commit

--- apache_deployment.sh
+++ apache_deployment.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 user="$USER"
-keyfile="/home/${user}/.ssh/ansible_key"
-ansible_path="/home/${user}/ansible/"
+keyfile="/home/$user/.ssh/ansible_key"
 
-playbook="${ansible_path}apache.yml"
+echo "Creating ansible folder"
+mkdir /home/$user/ansible
+
+ansible_path="/home/$user/ansible"
+
+echo "Copying playbook into ansible folder if it´s not already there"
+cp apache.yml $ansible_path/
+playbook="$ansible_path/apache.yml"
 
 read -p "Username of remote host: " remote_host
 echo "--ask-su-pass for sudo - leave blank for no additional parameters"
 read -p "zusätzliche Parameter: " par_var
 
-ansible-playbook -u ${remote_host} -i /etc/ansible/hosts ${par_var} --key-file=${keyfile} ${playbook}
+ansible-playbook -u $remote_host -i /etc/ansible/hosts $par_var --key-file=$keyfile $playbook
